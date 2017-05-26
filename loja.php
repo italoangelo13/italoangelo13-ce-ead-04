03450eeb8fc39c150ce67de3258481754ac57ab8
italoangelo13 <italoangelo.ti@gmail.com>
2017-05-26 11:32:14 -0300

    loja.php arrumada.

--- loja.php
+++ loja.php
@@ -1,28 +1,25 @@
-<?php  
-
-    session_start();
-    if(isset($_SESSION['logado'])){
-        if($_SESSION['adm'] == TRUE){
-            $nome = $_SESSION['nome'];
-        }
-        else{
-            $nome = $_SESSION['nome'];
-        }
+<?php
+session_start();
+if (isset($_SESSION['logado'])) {
+    if ($_SESSION['adm'] == TRUE) {
+        $nome = $_SESSION['nome'];
+    } else {
+        $nome = $_SESSION['nome'];
     }
-    else{
-        header("Location: index.html");
-    }
+} else {
+    header("Location: index.html");
+}
 
-    // DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
-    date_default_timezone_set('America/Sao_Paulo');
-    // CRIA UMA VARIAVEL E ARMAZENA A HORA ATUAL DO FUSO-HORÀRIO DEFINIDO (BRASÍLIA)
-    $dataLocal = date('d/m/Y');
-    $horaLocal = date('H:i:s', time());
-    require_once("conecta_mysql.inc.php");
+// DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
+date_default_timezone_set('America/Sao_Paulo');
+// CRIA UMA VARIAVEL E ARMAZENA A HORA ATUAL DO FUSO-HORÀRIO DEFINIDO (BRASÍLIA)
+$dataLocal = date('d/m/Y');
+$horaLocal = date('H:i:s', time());
+require_once("conecta_mysql.inc.php");
 
-    $sqlBuscaLivros = "SELECT l.titulo, c.categoria, l.autor, l.preco,l.qtde,l.codigo,l.imagem FROM livros as l inner join categorias as c on l.categoria = c.codigo order by codigo ASC";
+$sqlBuscaLivros = "SELECT l.titulo, c.categoria, l.autor, l.preco,l.qtde,l.codigo,l.imagem FROM livros as l inner join categorias as c on l.categoria = c.codigo order by codigo ASC";
 
-    $resultado = $mysqli -> query($sqlBuscaLivros);
+$resultado = $mysqli->query($sqlBuscaLivros);
 ?>
 
 <!doctype html>
@@ -32,36 +29,36 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Livraria do Italo</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/estilo2.css">
     </head>
     <body>
         <div class="container-fluid">
-        	<div class="row">
+            <div class="row">
                 <div class="jumbotron jumbotron-fluid col-lg-12 estilo-header">
-                  <div class="container">
-                    <div class="row">
-                        <div class="col-lg-10">
-                            <center>
-                                <h1 class="display-1">Livraria do Italo</h1>
-                                <p class="lead">Aqui voce encontra os Melhores Livros pelos menores preços!</p>
-                           </center>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <center>
+                                    <h1 class="display-1">Livraria do Italo</h1>
+                                    <p class="lead">Aqui voce encontra os Melhores Livros pelos menores preços!</p>
+                                </center>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <center><a href="logout.php" style="color: #fff; text-decoration: none;"> <img src="imagens/logout.ico" alt="Sair" width="70%"> </a></center>
+                            </div>
                         </div>
 
-                        <div class="col-lg-2">
-                            <center><a href="logout.php" style="color: #fff; text-decoration: none;"> <img src="imagens/logout.ico" alt="Sair" width="70%"> </a></center>
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <center><h4>Olá, <?php echo $nome; ?>!</h4></center>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <center><h6><?php echo $dataLocal; ?> <br> <?php echo $horaLocal; ?></h6></center>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="col-lg-10">
-                            <center><h4>Olá, <?php echo $nome; ?>!</h4></center>
-                        </div>
-
-                        <div class="col-lg-2">
-                            <center><h6><?php echo $dataLocal; ?> <br> <?php echo $horaLocal; ?></h6></center>
-                        </div>
-                    </div>
-                  </div>
                 </div>
             </div>
 
@@ -69,43 +66,58 @@
 
             <div class="row">
                 <h1 class="col-lg-12 alert alert-success" style="text-align: center;">
-                    Livros Mais Vendidos
+                    Os Melhores Livros, pelos menores preços!
                 </h1>
             </div>
-            
-            
+
+
 
             <div class="row">
+                <table class="table table-striped col-12">
+                    <thead>
+                        <tr>
+                            <th><center>Imagem</center></th>
+                            <th><center>Titulo</center></th>
+                            <th><center>Autor</center></th>
+                            <th><center>Categoria</center></th>
+                            <th><center>Preço</center></th>
+                            <th><center>Em Estoque</center></th>
+                            <th><center>Comprar Livro</center></th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         <?php
-                            while ($dados = $resultado->fetch_assoc()) {
-                                $titulo = $dados['titulo'];
-                                $categoria = $dados['categoria'];
-                                $autor = $dados['autor'];
-                                $imagem = $dados['imagem'];
-                                $preco = $dados['preco'];
-                                $quant = $dados['qtde'];
-                        ?>
-                            <div class="card col-lg-3" style="margin-top: 1%; margin-right: 4%; margin-left: 4%; margin-bottom: 4%; border: 2px solid #888; padding-top: 1%; border-radius: 0px 0px 50px 50px; background: -moz-linear-gradient(top, #7abcff 0%, #60abf8 44%, #4096ee 100%); /* FF3.6-15 */
-background: -webkit-linear-gradient(top, #7abcff 0%,#60abf8 44%,#4096ee 100%); color: #fff; box-shadow: 2px 5px 10px rgba(0,0,0,0.7);">
+                        while ($dados = $resultado->fetch_assoc()) {
+                            $titulo = $dados['titulo'];
+                            $categoria = $dados['categoria'];
+                            $autor = $dados['autor'];
+                            $imagem = $dados['imagem'];
+                            $preco = $dados['preco'];
+                            $quant = $dados['qtde'];
+                            ?>
 
-                              <img style="height: 404px; border-radius: 50px; border: 2px #fff solid;" class="card-img-top" src="<?php echo $imagem; ?>" alt="Card image cap">
-                              <div class="card-block">
-                                <hr style="background: #fff;">
-                                <h4 class="card-title"><?php echo $titulo; ?> - <small><?php echo $autor; ?></small></h4>
-                                <hr style="background: #fff;">
-                                <p class="card-text">Categoria: <?php echo $categoria; ?></p>
-                                <p class="card-text"><strong>Preço: R$<?php echo $preco; ?></strong></p>
-                                <p class="card-text"><label class="badge badge-success">Quantidade:</label> <?php echo $quant; ?> Livros</p>
-                                <hr style="background: #fff;">
-                                <center><button class="btn btn-success" type="submit">Comprar</button></center>
-                              </div>
-                            </div>
+                        <tr>
+                                <th><center><img class="img-loja" src="<?php echo $imagem; ?>" alt="<?php echo $titulo; ?>" title="<?php echo $titulo; ?>" /></center></th>
+                                <td scope="row"><center><h4><?php echo $titulo; ?></h4></center></td>
+                                <td><center><h4><?php echo $autor; ?></h4></center></td>
+                                <td><center><h4><?php echo $categoria; ?></h4></center></td>
+                                <td><center><h4><?php echo $preco; ?></h4></center></td>
+                                <td><center><h4><?php echo $quant; ?></h4></center></td>
+                                <td><center><h4>
+                                    <form>
+                                        <input style="width: 70px;" class="form-control-sm" type="number" name="quant" placeholder="0" /> <button class="btn btn-outline-success" type="submit" >Adcionar ao Carrinho </button>     
+                                    </form>
+                                </h4></center></td>
+                            </tr>
 
-                        <?php
-                            }
+
+                            <?php
+                        }
                         ?>
+                    </tbody>
+                </table>
             </div>
-            
+
             <footer class="row footer">
                 <div class="col-lg-12 estilo-footer">
                     <center><h1>@Livrariadoitalo</h1></center>
